da943dfeae6ea80407ac28c8475343ce7a61cf15
Evgeniy Zalevsky <bikkel@Evgeniys-MacBook-Pro.local>
2020-12-25 22:32:18 +0200

    migrate to php-di 6

--- di-config.php
+++ di-config.php
@@ -1,13 +1,13 @@
 <?php
 namespace NewsParserPlugin;
-use function DI\object;
+use function DI\create;
 use function DI\get;
 
 return array(
-    Utils\MenuConfig::class=>object(Utils\MenuConfig::class)->constructor(NEWS_PARSER_PLUGIN_DIR.'menu-config.php'),
-    Parser\HTMLPatternParser::class=>object(Parser\HTMLPatternParser::class)->constructor(get(Utils\AdapterGutenberg::class)),
-    Menu\Admin\MenuPage::class=>object(Menu\Admin\MenuPage::class)->constructor(get(Utils\MenuConfig::class)),
-    Controller\PostController::class=>object(Controller\PostController::class)->constructor(get(Parser\HTMLPatternParser::class),get(Utils\ResponseFormatter::class)),
-    Controller\ListController::class=>object(Controller\ListController::class)->constructor(get(Parser\XMLParser::class),get(Utils\ResponseFormatter::class)),
-    Controller\VisualConstructorController::class=>object(Controller\VisualConstructorController::class)->constructor(get(Parser\HTMLRaw::class),get(Utils\ResponseFormatter::class))
+    Utils\MenuConfig::class=>create(Utils\MenuConfig::class)->constructor(NEWS_PARSER_PLUGIN_DIR.'menu-config.php'),
+    Parser\HTMLPatternParser::class=>create(Parser\HTMLPatternParser::class)->constructor(get(Utils\AdapterGutenberg::class)),
+    Menu\Admin\MenuPage::class=>create(Menu\Admin\MenuPage::class)->constructor(get(Utils\MenuConfig::class)),
+    Controller\PostController::class=>create(Controller\PostController::class)->constructor(get(Parser\HTMLPatternParser::class),get(Utils\ResponseFormatter::class)),
+    Controller\ListController::class=>create(Controller\ListController::class)->constructor(get(Parser\XMLParser::class),get(Utils\ResponseFormatter::class)),
+    Controller\VisualConstructorController::class=>create(Controller\VisualConstructorController::class)->constructor(get(Parser\HTMLRaw::class),get(Utils\ResponseFormatter::class))
 );
--- news-parser.php
+++ news-parser.php
@@ -3,7 +3,7 @@
 Plugin Name: News-Parser
 Plugin URI: https://github.com/zalevsk1y/news-parser
 Description: Parse full text news from RSS Feed
-Version: 1.0.2
+Version: 1.0.3
 Author: Evgeny S.Zalevskiy <2600@ukr.net>
 Author URI: https://github.com/zalevsk1y/
 License: MIT
@@ -14,7 +14,7 @@ Text Domain: news-parser
 namespace NewsParserPlugin;
 
 
-define('NEWS_PARSER_PLUGIN_VERSION', '1.0.2');
+define('NEWS_PARSER_PLUGIN_VERSION', '1.0.3');
 define("NEWS_PARSER_PLUGIN_SLUG", 'news-parser');
 define("NEWS_PARSER_PLUGIN_ROOT_NAMESPACE", 'NewsParserPlugin');
 define("NEWS_PARSER_PLUGIN_SETTINGS_SLUG", 'news_parser_settings');
