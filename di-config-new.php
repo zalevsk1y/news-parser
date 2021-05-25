a08c914e3dbe1f9647ebfea5abfe58afd6012b77
Evgeniy S.Zalevsky <2600@ukr.net>
2021-05-25 16:26:50 +0300

    fixed DI config error. TemplateController was not in di-config array

--- di-config-new.php
+++ di-config-new.php
@@ -10,6 +10,7 @@ return array(
     Parser\HTMLRaw::class=>[],
     Parser\HTMLPatternParser::class=>[Utils\AdapterGutenberg::class],
     Menu\Admin\MenuPage::class=>[Utils\MenuConfig::class],
+    Controller\TemplateController::class=>[Utils\ResponseFormatter::class],
     Controller\PostController::class=>[Parser\HTMLPatternParser::class,Utils\ResponseFormatter::class],
     Controller\ListController::class=>[Parser\XMLParser::class,Utils\ResponseFormatter::class],
     Controller\VisualConstructorController::class=>[Parser\HTMLRaw::class,Utils\ResponseFormatter::class]
